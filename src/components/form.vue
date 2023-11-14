c9c2c18456e0cc712af4ea8efd4b1a1ef1a70116
Etienne Naude <eti@naude.dev>
2023-11-14 19:36:59 +0000

    firebase auth

--- src/components/form.vue
+++ src/components/form.vue
@@ -3,9 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import firebaseConfig from "./firebase";
 import { defineComponent } from "vue";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import './form.css';
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth();
 
 class Field {
   name: string;
@@ -29,6 +33,7 @@ export default defineComponent({
         style: "loading",
         shown: false,
       },
+      authorised: false,
       items: [
         new Field("Educational"),
         new Field("Financial"),
@@ -43,12 +48,6 @@ export default defineComponent({
     async submitForm() {
       let valid = true;
 
-      this.banner = {
-        text: "Loading...",
-        style: "loading",
-        shown: true,
-      };
-
       const dateStamp = new Date();
 
       let data: any = { dateStamp };
@@ -59,11 +58,7 @@ export default defineComponent({
 
         if (item.plan === "" || item.doing === "") {
           valid = false;
-          this.banner = {
-            text: "Fill Everything In!",
-            style: "error",
-            shown: true,
-          };
+          this.showBanner("Fill Everything In!", "error");
         }
 
         data[item.name] = {
@@ -75,23 +70,62 @@ export default defineComponent({
       }
 
       try {
+        if (!this.authorised) {
+          this.showBanner("No! bad! No Auth!", "error");
+          return;
+        }
         if (valid) {
           const docRef = await addDoc(collection(db, "feels"), data);
           console.log("Document written with ID: ", docRef.id);
-          this.banner = {
-            text: "Form submitted successfully!",
-            style: "success",
-            shown: true,
-          };
+          this.showBanner("Form submitted successfully!", "success");
         }
       } catch (e) {
         console.error("Error adding document: ", e);
+        this.showBanner("Error submitting form!", "error");
       }
+    },
+
+    async authorise() {
+
+      signInWithPopup(auth, new GoogleAuthProvider())
+        .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential!.accessToken;
+          const user = result.user;
+
+          if (user.uid.substring(0, 5) != "WFuEx") {
+            this.showBanner("Sorry this site is only for me :)", "error");
+            return;
+          }
+
+          this.showBanner("Hey ;)", "success");
+          this.authorised = true;
+          console.log(token, user);
+        }).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          const email = error.customData.email;
+          const credential = GoogleAuthProvider.credentialFromError(error);
+
+          console.error(errorCode, errorMessage, email, credential)
+          this.authorised = false;
+          this.showBanner("Auth", "error");
+        });
+    }, showBanner(text: string, style: string) {
+      this.banner.text = text;
+      this.banner.style = style;
+      this.banner.shown = true;
+
 
       setTimeout(() => {
         this.banner.shown = false;
       }, 5000);
+
     },
+  },
+
+  beforeMount() {
+    this.authorise()
   },
 });
 </script>
@@ -102,25 +136,9 @@ export default defineComponent({
       <div v-for="item in items">
         <div class="card shadow">
           <h3>{{ item.name }}</h3>
-          <textarea
-            placeholder="Currently Doing..."
-            v-model="item.doing"
-            class="large"
-            spellcheck="false"
-          />
-          <input
-            class="slider"
-            type="range"
-            min="1"
-            max="5"
-            v-model="item.score"
-          />
-          <textarea
-            placeholder="Will Do..."
-            v-model="item.plan"
-            class="large"
-            spellcheck="false"
-          />
+          <textarea placeholder="Currently Doing..." v-model="item.doing" class="large" spellcheck="false" />
+          <input class="slider" type="range" min="1" max="5" v-model="item.score" />
+          <textarea placeholder="Will Do..." v-model="item.plan" class="large" spellcheck="false" />
         </div>
       </div>
     </div>
@@ -132,184 +150,3 @@ export default defineComponent({
     {{ banner.text }}
   </div>
 </template>
-
-<style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap");
-
-:root {
-  --primary: #4fffc4;
-}
-
-* {
-  --primary: #4fffc4;
-
-  color: #eee;
-  user-select: none;
-  font-family: "Roboto", sans-serif;
-  transition: ease 0.2s;
-}
-
-.shadow {
-  box-shadow: 2px 2px 0 1px #0008;
-}
-
-.background {
-  background: #333;
-  overflow: auto;
-  display: flex;
-  justify-content: center;
-}
-
-.flex-cont {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  width: min(100%, 1600px);
-}
-
-.card {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  padding: 0 20px;
-  margin: 20px;
-  width: min(400px, 90vw);
-  border-radius: 10px;
-  background: #252525;
-}
-
-.card > * {
-  width: 100%;
-  padding: 0;
-  margin: 10px 0;
-}
-
-textarea {
-  height: 200px;
-  border: 2px solid var(--primary);
-  outline: none;
-  padding: 5px;
-  background: transparent;
-  border-radius: 10px;
-  resize: none;
-  transition: ease 0.2s;
-  user-select: all;
-  font-size: 16px;
-}
-
-textarea:hover,
-textarea:focus {
-  background: #333;
-  border: 2px solid #4da9ff;
-}
-
-textarea::placeholder {
-  color: #888;
-  margin: 2px;
-  padding: 5px;
-}
-
-h3 {
-  font-weight: bold;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-  font-size: 2em;
-}
-
-.slider:hover {
-  opacity: 1;
-}
-
-.slider {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 100%;
-  height: 25px;
-  background: #111;
-  opacity: 0.7;
-  transition: opacity 0.2s;
-  cursor: pointer;
-  border-radius: 100vmax;
-}
-
-.slider::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 25px;
-  height: 25px;
-  border-radius: 100vmax;
-  background: var(--primary);
-  cursor: pointer;
-}
-
-::-webkit-scrollbar {
-  width: 10px;
-  border-radius: 100vmax;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #222;
-  border-radius: 100vmax;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 100vmax;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #fff;
-}
-
-button {
-  position: fixed;
-  bottom: 10px;
-  left: 50%;
-  transform: translate(-50%, 0);
-  width: min(80%, 600px);
-  height: 50px;
-  background: var(--primary);
-  border: none;
-  color: #222;
-  font-size: 1.5em;
-  font-weight: bold;
-  cursor: pointer;
-  border-radius: 10px;
-}
-
-.banner {
-  padding: 1rem;
-  border-radius: 10px;
-  text-align: center;
-  font-weight: bold;
-
-  position: fixed;
-  top: 10px;
-  left: 50%;
-  transform: translate(-50%, 0);
-  width: 90%;
-  background-color: #111;
-}
-
-.banner.success {
-  border: 10px solid var(--primary);
-  color: var(--primary);
-}
-
-.banner.error {
-  border: 10px solid #ff4f4f;
-  color: #ff4f4f;
-}
-
-.banner.loading {
-  border: 10px solid #4da9ff;
-  color: #4da9ff;
-}
-</style>
